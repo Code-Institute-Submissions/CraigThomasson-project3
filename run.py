1e7284e474e334f288848f8bbf2c4ee17abea836
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2021-11-07 18:55:03 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+class Entity:
+    def __init__(self, health, attack, name, weapon, items):
+        self.health = health
+        self.attack = attack
+        self.name = name
+        self.weapon = weapon
+        self.items = items
