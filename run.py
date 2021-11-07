3bd0a127681a664b0e7368fb0f885fce3e1cb760
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2021-11-07 20:15:49 +0000

    add player sublass and test it works

--- run.py
+++ run.py
@@ -1,7 +1,21 @@
 class Entity:
-    def __init__(self, health, attack, name, weapon, items):
+    def __init__(self, health, attack, name):
         self.health = health
         self.attack = attack
         self.name = name
+
+class Player(Entity):
+    def __init__(self, weapon, items):
+        Entity.__init__(self, 10, 3, "player")
         self.weapon = weapon
         self.items = items
+
+
+dave = Player("knife", ["bread", "clothes"])
+
+print("daves weapin", dave.weapon)
+print("daves items", dave.items)
+print("daves health is", dave.health)
+
+dave.weapon = "sword"
+print("daves new weapon is", dave.weapon)
