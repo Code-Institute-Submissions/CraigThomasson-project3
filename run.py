1f8f2d89bc7373104674ab05b0b162edfab7a401
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2021-11-09 00:37:34 +0000

    test player class and gonlin class

--- run.py
+++ run.py
@@ -12,19 +12,24 @@ class Player(Entity):
 
 class Goblin(Entity):
     def __init__(self, items):
-        Entity.__init__(self, 5, 2, "Golin")
+        Entity.__init__(self, 5, 2, "Goblin", "scimatar")
         self.items = items
+        items = []
 
-class Goblin_archer(Goblin):
-    Entity.__init__(self, ["torch", "rotting meat"])
-    weapon = "bow"
-
+class goblin_archer(Goblin):
+    def __init__(self):
+        Goblin.__init__(self, [])
 
 dave = Player(["bread", "clothes"])
 
-print("daves weapin", dave.weapon)
+print("daves weapon", dave.weapon)
 print("daves items", dave.items)
 print("daves health is", dave.hp)
 
 dave.weapon = "sword"
 print("daves new weapon is a", dave.weapon)
+dave.name = input("enter your name:")
+print(dave.name)
+enemy = goblin_archer()
+enemy.weapon = "bow"
+print(enemy.weapon)
