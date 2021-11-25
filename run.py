5c3bfe480f0566e15dc513bb458ad1008b3a1577
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2021-11-25 23:47:18 +0000

    add battle choice function, add change weapon method to weapon class.

--- run.py
+++ run.py
@@ -6,13 +6,13 @@ class Entity:
     """
     base class for all player and enemy classes 
     """
-    def __init__(self, health, attack, speed, name, weapon, weapon_list):
+    def __init__(self, health, attack, speed, name, weapon, weapon_inventory):
         self.health = health
         self.attack = attack
         self.speed = speed
         self.name = name
         self.weapon = weapon
-        self.weapon_list = weapon_list
+        self.weapon_inventory = weapon_inventory
 
     def make_atk(self): 
         """
@@ -25,9 +25,12 @@ class Entity:
         """
         allows the player to see weapon list and change equipped weapon
         """
-        print(f'your current weapon list is: {self.weapon_list}. {self.weapon.name} is equipped')
+        weapon_list = []
+        for weapon in self.weapon_inventory:
+            weapon_list.append(weapon.name)
+        print(f'your current weapon list is: {weapon_list}. {self.weapon.name} is equipped')
         weapon_choice = input(f'type the  name of the weapon you would like to equip: ')
-        for weapon in self.weapon_list:
+        for weapon in self.weapon_inventory:
             if weapon.name == weapon_choice:
                 self.weapon = weapon
 
@@ -37,7 +40,7 @@ class Player(Entity):
     This will handel all stats and info for the players character
     """
     def __init__(self, items):
-        Entity.__init__(self, 17, 5, 5, "Player", Rusty_knife(), ["Rusty_knife()"])
+        Entity.__init__(self, 17, 5, 5, "Player", Rusty_knife(), [Rusty_knife(), Scimitar()])
         self.items = items
 
 class Goblin(Entity):
@@ -45,7 +48,7 @@ class Goblin(Entity):
     Base class for all goblin type enemies
     """
     def __init__(self, items):
-        Entity.__init__(self, 10, 2, 6, "Goblin", Scimitar(), ["Scimitar()"])
+        Entity.__init__(self, 10, 2, 6, "Goblin", Scimitar(), [Scimitar()])
         self.items = items
         items = []
 
@@ -100,6 +103,7 @@ def battle(player_character, enemy):
             if player_character.health <= 0:
                 print(f"you have been slain by the {enemy.name}....\n")
                 player_character = get_player_name()
+            battle_option(player_character, enemy)
 
 def battle_option(player, enemy):
     """
@@ -108,8 +112,10 @@ def battle_option(player, enemy):
     print(f"you are in combat with a {enemy.name} you can: Attack or change weapon")
     choice = input("what will you do?:...\n To attack type: option 1\n To change weapon type: option 2: ")
     if choice == "option 1":
-        break
+        print("atck option")
     if choice == "option 2":
+        print("weapon option")
+        player.change_weapon()
 
 
 # story functions
@@ -119,7 +125,7 @@ def goblin_ambush(player_character):
 
 def ambush_goblin(player_character):
     print("as you sneak around you notice a goblin hiding behind a bush watching the road\n")
-    choice = input("what would you liek to do?\n type 'option 1' to attack\n type 'option 2' to sneak past....\n:")
+    choice = input("what would you like to do?\n type 'option 1' to attack\n type 'option 2' to sneak past....:\n")
     if choice == "option 1":
         battle(player_character, Goblin("chalk, rabit"))
     if choice == "option 2":
@@ -132,6 +138,8 @@ def get_player_name():
     player_character = Player(["fire starter"])
     player_character.name = input("hello traveller what is your name?: ")
     print(f'well met {player_character.name}! take care on these roads there are goblins on the loose.\n')
+
+    battle(player_character, Goblin("chalk, rabit"))
     return player_character
 
 def introduction(player_character):
@@ -145,7 +153,7 @@ def introduction(player_character):
 
 player_character = get_player_name()
 
-introduction(player_character)
+# introduction(player_character)
 
 
 
