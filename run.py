9203f7a45fb9e5b615bf8da07d63bf7bbdeb1e30
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2021-12-13 13:08:34 +0000

    change all print ststments to delay_print

--- run.py
+++ run.py
@@ -110,7 +110,7 @@ def delay_print(s):
     for c in s:
         sys.stdout.write(c)
         sys.stdout.flush()
-        time.sleep(0.25)
+        time.sleep(0.09)
 
 
 def weapon_name_list(weapon):
